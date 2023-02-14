a4f764db00dbd8b5c46a0601cf1caf42b31267b0
Tinmarian <tinmar96@gmail.com>
2023-02-14 10:48:58 -0600

    Creation and use of Plugins in Airflow; Example

--- plugins/sql_plugin.py
+++ plugins/sql_plugin.py
@@ -1,1 +1,26 @@
-from airiflow.
+from airiflow.plugins_manager import AirflowPlugin
+from airflow.models import BaseOperator
+from airflow.hooks.base_hook import BaseHook
+
+class SqlOperator(BaseOperator):
+    algo
+
+class SqlHook(BaseHook):
+    algo
+
+class SqlPlugin(AirflowPlugin):
+    name = 'sql_plugin'
+    operators = [SqlOperator]
+    hooks = [SqlHook]
+    executors = []
+    macros = []
+    admin_views = []
+    flask_blueprints = []
+    menu_links = []
+
+
+# Una vez guardado el archivo en la carpeta de plugins, podemos llamar a los nuevos objetos
+# de la siguiente manera:
+
+    # from airflow.operators.sql_plugin import SqlOperator
+    # from airflow.hooks.sql_plugin import SqlHook
